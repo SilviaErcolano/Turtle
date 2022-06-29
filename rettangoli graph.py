1c59263ced8c04e233ea8d374d56fa97960ee8cb
SilviaErcolano <si.ercolano@gmail.com>
2022-06-29 19:50:18 +0200

    Add files via upload

---
+++ rettangoli graph.py
@@ -0,0 +1,14 @@
+from turtle import *
+t=Turtle()
+s=Screen()
+for i in range (0,40):
+    t.forward(100)
+    t.right(90)
+    t.forward(150)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(150)
+    
+    t.right(80)
+s.mainloop()
