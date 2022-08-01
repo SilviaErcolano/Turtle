7eb0965810aa493b5e85abebe92cb201e9875ba0
SilviaErcolano <si.ercolano@gmail.com>
2022-08-01 16:05:49 +0200

    Add files via upload

---
+++ screen.py
@@ -0,0 +1,13 @@
+from turtle import *
+s=Screen()
+t=Turtle()
+s.colormode(255)
+R=153
+G=102
+B=51
+s.bgcolor((R,G,B))
+""""
+Riferimento gradazione colore
+https://www.w3schools.com/colors/colors_picker.asp
+"""""
+s.mainloop()
