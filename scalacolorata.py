e13c41c6c28173191e31e3bdc8fe372a367b5a40
SilviaErcolano <si.ercolano@gmail.com>
2022-08-01 16:02:56 +0200

    Add files via upload

---
+++ scalacolorata.py
@@ -0,0 +1,11 @@
+from turtle import *
+p=Turtle()
+for i in range(10):
+    p.color("red")
+    p.forward(20)
+    p.right(90)
+    p.color("blue")
+    p.forward(20)
+    p.left(90)
+    p.color("yellow")
+    p.forward(20)
